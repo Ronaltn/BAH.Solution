8407cf62bd6e8046c9f584d62b6ea1579df49f85
Martin Wang <ronaltn@live.cn>
2016-03-25 16:20:56 +0800

    加入ToType和ToTypeOrDefault方法，准备完善注释。

--- BAH.BOS.Extension/ObjectExtension.cs
+++ BAH.BOS.Extension/ObjectExtension.cs
@@ -5,8 +5,28 @@ using System.Text;
 
 namespace System
 {
+    /// <summary>
+    /// Objectd对象的扩展类。
+    /// </summary>
     public static class ObjectExtension
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static T ToType<T>(this object obj)
+        {
+            return (T)obj;
+        }//end method
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public static T ToChangeType<T>(this object obj)
         {
             return (T)Convert.ChangeType(obj, typeof(T));
@@ -16,11 +36,39 @@ namespace System
         /// 匿名对象转换专用。
         /// </summary>
         /// <url>http://www.cnblogs.com/tianxiang2046/p/3586537.html</url>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="t">匿名对象的初始化结构，例如：new{ Name = string.Empty, Age = 0 }。</param>
+        /// <returns></returns>
         public static T ToAnonymousType<T>(this object obj, T t)
         {
             return (T)obj;
         }//end method
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static T ToTypeOrDefault<T>(this object obj)
+        {
+            try
+            {
+                return ToType<T>(obj);
+            }
+            catch
+            {
+                return default(T);
+            }
+        }//end method
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public static T ToChangeTypeOrDefault<T>(this object obj)
         {
             try
@@ -37,6 +85,10 @@ namespace System
         /// 匿名对象转换专用。
         /// </summary>
         /// <url>http://www.cnblogs.com/tianxiang2046/p/3586537.html</url>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="t">匿名对象的初始化结构，例如：new{ Name = string.Empty, Age = 0 }。</param>
+        /// <returns></returns>
         public static T ToAnonymousTypeOrDefault<T>(this object obj, T t)
         {
             try
@@ -48,5 +100,6 @@ namespace System
                 return default(T);
             }
         }//end method
+
     }//end class
 }//end namespace
