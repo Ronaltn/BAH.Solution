9ec31471d8d3d1f5208a59fcda374b00a0dd7b1e
Martin Wang <ronaltn@live.cn>
2016-06-23 21:22:36 +0800

    新增用户服务

---
+++ BAH.BOS.WebAPI.ServiceStub/User/UserService.cs
@@ -0,0 +1,22 @@
+﻿using Kingdee.BOS.ServiceFacade.KDServiceFx;
+using Kingdee.BOS.WebApi.ServicesStub;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BAH.BOS.WebAPI.ServiceStub.User
+{
+    /// <summary>
+    /// 用户服务。
+    /// </summary>
+    public class UserService : AbstractWebApiBusinessService
+    {
+        public UserService(KDServiceContext context)
+            : base(context)
+        { 
+            
+        }//end constructor
+
+    }//end class
+}//end namespace
