9761751fe2dde5f98af1bbb07c82da62ade0f64a
Martin Wang <ronaltn@live.cn>
2016-06-23 10:17:18 +0800

    增加WebAPI的业务结果返回定义

---
+++ BAH.BOS.WebAPI.ServiceStub/BusinessOperationResult/ServiceResult.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BAH.BOS.WebAPI.ServiceStub.BusinessOperationResult
+{
+    /// <summary>
+    /// 服务结果。
+    /// </summary>
+    [JsonObject]
+    public class ServiceResult
+    {
+        [JsonProperty]
+        public int Code { get; set; }//end property
+
+        [JsonProperty]
+        public string Message { get; set; }//end property
+
+    }//end class
+
+    /// <summary>
+    /// 服务结果，定义Body为泛型。
+    /// </summary>
+    /// <typeparam name="T">泛型定义。</typeparam>
+    [JsonObject]
+    public class ServiceResult<T> : ServiceResult
+    {
+        /// <summary>
+        /// 主体对象，类型由泛型定义。
+        /// </summary>
+        [JsonProperty]
+        public T Body { get; set; }//end property
+    }//end class
+
+}//end namespace
