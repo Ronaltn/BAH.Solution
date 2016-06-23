0ff7b2284e89d09c4ff94507a65fe5fdee2128cc
Martin Wang <ronaltn@live.cn>
2016-06-23 10:51:20 +0800

    添加会话服务

---
+++ BAH.BOS.WebAPI.ServiceStub/BusinessOperationResult/ResultCode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BAH.BOS.WebAPI.ServiceStub.BusinessOperationResult
+{
+    public enum ResultCode
+    {
+        Success,
+        Fail
+    }//end enum
+}//end namespace
---
+++ BAH.BOS.WebAPI.ServiceStub/SessionService.cs
@@ -0,0 +1,37 @@
+﻿using BAH.BOS.WebAPI.ServiceStub.BusinessOperationResult;
+using Kingdee.BOS;
+using Kingdee.BOS.ServiceFacade.KDServiceFx;
+using Kingdee.BOS.WebApi.ServicesStub;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BAH.BOS.WebAPI.ServiceStub
+{
+    /// <summary>
+    /// 会话服务。
+    /// </summary>
+    public class SessionService : AbstractWebApiBusinessService
+    {
+        public SessionService(KDServiceContext context)
+            : base(context)
+        { 
+            
+        }//end constructor
+
+        /// <summary>
+        /// 接收心跳信号，返回服务端结果。
+        /// </summary>
+        /// <returns>返回服务端结果。</returns>
+        public virtual ServiceResult Heartbeat()
+        {
+            return new ServiceResult
+            {
+                Code = (int)ResultCode.Success,
+                Message = string.Format("{0}，服务端已收到你的心跳信号！", this.KDContext.Session.AppContext.SessionId)
+            };
+        }//end method
+
+    }//end class
+}//end namespace
