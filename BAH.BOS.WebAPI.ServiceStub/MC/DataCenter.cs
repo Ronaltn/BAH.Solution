b42a4e544bce11cec6ccd92ebd609414d7e1dd73
Martin Wang <ronaltn@live.cn>
2016-08-07 16:27:41 +0800

    添加获取业务数据中心方法

---
+++ BAH.BOS.WebAPI.ServiceStub/MC/DataCenter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BAH.BOS.WebAPI.ServiceStub.MC
+{
+    public class DataCenter
+    {
+        public virtual string Id { get; set; }
+
+        public virtual string Name { get; set; }
+    }//end class
+}//end namespace
---
+++ BAH.BOS.WebAPI.ServiceStub/MC/MCService.cs
@@ -0,0 +1,48 @@
+﻿using Kingdee.BOS;
+using Kingdee.BOS.Contracts;
+using Kingdee.BOS.MC.ServiceFacade.KDServiceClient;
+using Kingdee.BOS.WebApi.ServicesStub;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BAH.BOS.WebAPI.ServiceStub.MC
+{
+    /// <summary>
+    /// 管理中心服务。
+    /// </summary>
+    public class MCService : AbstractWebApiBusinessService
+    {
+        /// <summary>
+        /// 获取业务数据中心。
+        /// </summary>
+        /// <returns>返回服务端结果。</returns>
+        public virtual ServiceResult<List<DataCenter>> GetBusinessDataCenters()
+        {
+            var result = new ServiceResult<List<DataCenter>>();
+
+            try
+            {
+                var murl = KDConfiguration.Current.ManagementSiteUrl;
+                var service = ServiceFactory.GetDataCenterService(murl);
+                var mctx = service.GetManagementDataCenterContext();
+                var infos = service.GetAllBusinessDataCenterInfo(mctx);
+
+                result.Code = (int)ResultCode.Success;
+                result.Data = infos.Select(db => new DataCenter 
+                {
+                    Id = db.DataCenterID, 
+                    Name = db.DataCenterName.ToString() 
+                }).ToList();
+            }
+            catch
+            {
+                result.Code = (int)ResultCode.Fail;
+            }
+
+            return result;
+        }//end method
+
+    }//end class
+}//end namespace
