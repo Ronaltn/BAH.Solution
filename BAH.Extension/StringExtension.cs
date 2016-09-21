f501b816f270c9bc127defbc07f20ac28c8ddc24
Martin Wang <ronaltn@live.cn>
2016-09-22 00:20:14 +0800

    增加字符串为null时返回empty的扩展方法

---
+++ BAH.Extension/StringExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class StringExtension
+    {
+        public static string EmptyWhenNull(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str) ? string.Empty : str;
+        }
+    }
+}
