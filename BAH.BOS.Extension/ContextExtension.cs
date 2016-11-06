bd8e8a53186b23dc8bc52986c7c76cfd62d5a49f
Martin Wang <ronaltn@live.cn>
2016-11-06 13:45:09 +0800

    增添CreateContext方法。

--- BAH.BOS.Extension/ContextExtension.cs
+++ BAH.BOS.Extension/ContextExtension.cs
@@ -1,6 +1,8 @@
 ﻿using Kingdee.BOS;
 using Kingdee.BOS.Contracts;
 using Kingdee.BOS.Core;
+using Kingdee.BOS.Core.Metadata;
+using Kingdee.BOS.Orm.DataEntity;
 using Kingdee.BOS.ServiceHelper;
 using System;
 using System.Collections.Generic;
@@ -11,6 +13,23 @@ namespace Kingdee.BOS
 {
     public static class ContextExtension
     {
+        public static Context Create(this Context ctx, string dataCenterId, long userId)
+        {
+            Context contextByDataCenterId = DataCenterService.GetDataCenterContextByID(dataCenterId);
+
+            //处理用户登录名
+            {
+                FormMetadata metadata = FormMetaDataCache.GetCachedFormMetaData(contextByDataCenterId, FormIdConst.SEC_User);
+                BusinessInfo businessInfo = metadata.BusinessInfo.GetSubBusinessInfo(new List<string> { "FNumber", "FName" });
+                DynamicObject dataObject = BusinessDataServiceHelper.LoadSingle(ctx, userId, businessInfo.GetDynamicObjectType());
+                contextByDataCenterId.LoginName = dataObject.FieldProperty<string>(businessInfo.GetField("FNumber"));
+                contextByDataCenterId.UserName = dataObject.FieldProperty<LocaleValue>(businessInfo.GetField("FName")).Value(contextByDataCenterId);
+            }
+
+            if (ctx == null) ctx = contextByDataCenterId;
+            return contextByDataCenterId;
+        }//end static method
+
         public static Context CreateAdministrator(this Context ctx, string dataCenterId)
         {
             var conf = KDConfiguration.Current;
