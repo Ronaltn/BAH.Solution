bdf6a4dac89bb3ac59c9dbb8650c3ed1c78de03d
Martin Wang <ronaltn@live.cn>
2016-11-07 13:38:49 +0800

    增加DateTime扩展

---
+++ BAH.Extension/DateTimeExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class DateTimeExtension
+    {
+        public static DateTime FirstDayOfMonth(this DateTime datetime)
+        {
+            return datetime.AddDays(1 - datetime.Day);
+        }
+
+        public static DateTime LastDayOfMonth(this DateTime datetime)
+        {
+            return datetime.AddDays(1 - datetime.Day).AddMonths(1).AddDays(-1);
+        }
+    }
+}
