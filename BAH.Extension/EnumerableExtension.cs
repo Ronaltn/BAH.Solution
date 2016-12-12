dd750adaa3350a78b91ab184ed868d02b97b30c0
Martin Wang <ronaltn@live.cn>
2016-12-12 10:58:35 +0800

    删除无意义的方法 对分页方法添加直接取数

--- BAH.Extension/EnumerableExtension.cs
+++ BAH.Extension/EnumerableExtension.cs
@@ -7,49 +7,33 @@ namespace System.Linq
 {
     public static class EnumerableExtension
     {
-        public static bool AnyOrNull<TSource>(this IEnumerable<TSource> source)
+        public static IList<IEnumerable<TSource>> Paging<TSource>(this IEnumerable<TSource> source, int size)
         {
-            return source == null ? false : source.Any();
-        }//end method
+            if (size < 0) throw new ArgumentOutOfRangeException();
 
-        public static bool AnyOrNull<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
-        {
-            return source == null ? false : source.Any(predicate);
-        }//end method
-
-        public static TSource FirstOrNullDefault<TSource>(this IEnumerable<TSource> source)
-        {
-            return AnyOrNull<TSource>(source) ? source.FirstOrDefault() : default(TSource);
-        }//end method
-
-        public static TSource FirstOrNullDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
-        {
-            return AnyOrNull<TSource>(source) ? source.FirstOrDefault(predicate) : default(TSource);
-        }//end method
-
-        public static TSource SingleOrNullDefault<TSource>(this IEnumerable<TSource> source)
-        {
-            return AnyOrNull<TSource>(source) ? source.SingleOrDefault() : default(TSource);
-        }//end method
-
-        public static TSource SingleOrNullDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
-        {
-            return AnyOrNull<TSource>(source) ? source.SingleOrDefault(predicate) : default(TSource);
-        }//end method
-
-        public static IList<IEnumerable<TSource>> Paging<TSource>(this IEnumerable<TSource> source, int limit)
-        {
             IList<IEnumerable<TSource>> result = new List<IEnumerable<TSource>>();
-            int num = 1, left = source.Count();
+            int index = 0, left = source.Count();
             while (left > 0)
             {
-                result.Add(source.Skip((num - 1) * limit).Take(left >= limit ? limit : left));
+                result.Add(source.Skip(index * size).Take(left >= size ? size : left));
 
-                num++;
-                left = left - limit;
+                index++;
+                left = left - size;
             }//end while
             return result;
-        }//end method
+        }
+
+        public static IEnumerable<TSource> Paging<TSource>(this IEnumerable<TSource> source, int size, int index)
+        {
+            if (size < 0) throw new ArgumentOutOfRangeException();
+            if (index < 0) throw new ArgumentOutOfRangeException();
+
+            int has = index * size;
+            int left = source.Count() - has;
+            left = left >= 0 ? left : 0;
+            IEnumerable<TSource> result = source.Skip(has).Take(left >= size ? size : left);
+            return result;
+        }
 
     }//end class
 }//end namespace
