50299a4b146a15941ac6a7bca1508eae71f9d996
Martin Wang <ronaltn@live.cn>
2016-12-21 21:38:12 +0800

    Context增加从缓存获取的方法。

--- BAH.BOS.Extension/ContextExtension.cs
+++ BAH.BOS.Extension/ContextExtension.cs
@@ -28,18 +28,40 @@ namespace Kingdee.BOS
                 contextByDataCenterId.UserName = dataObject.FieldProperty<string>(businessInfo.GetField("FName"));
             }
 
-            if (ctx == null) ctx = contextByDataCenterId;
+            return contextByDataCenterId;
+        }//end static method
+
+        public static Context CreateInstanceFromCache(this Context ctx, string dataCenterId, long userId)
+        {
+            Context contextByDataCenterId = DataCenterService.GetDataCenterContextFromCache(dataCenterId);
+
+            //处理用户登录名
+            {
+                FormMetadata metadata = FormMetaDataCache.GetCachedFormMetaData(contextByDataCenterId, FormIdConst.SEC_User);
+                BusinessInfo businessInfo = metadata.BusinessInfo.GetSubBusinessInfo(new List<string> { "FNumber", "FUserAccount", "FName" });
+                DynamicObject dataObject = BusinessDataServiceHelper.LoadFromCache(contextByDataCenterId, new object[] { userId }, businessInfo.GetDynamicObjectType()).FirstOrDefault();
+                contextByDataCenterId.UserId = dataObject.PkId<long>();
+                if (businessInfo.GetField("FNumber") != null) contextByDataCenterId.LoginName = dataObject.FieldProperty<string>(businessInfo.GetField("FNumber"));
+                if (businessInfo.GetField("FUserAccount") != null) contextByDataCenterId.LoginName = dataObject.FieldProperty<string>(businessInfo.GetField("FUserAccount"));
+                contextByDataCenterId.UserName = dataObject.FieldProperty<string>(businessInfo.GetField("FName"));
+            }
+
             return contextByDataCenterId;
         }//end static method
 
         public static Context CreateAdministrator(this Context ctx, string dataCenterId)
         {
-            var conf = KDConfiguration.Current;
             Context contextByDataCenterId = DataCenterService.GetDataCenterContextByID(dataCenterId);
             contextByDataCenterId.UserId = FormConst.AdministratorID;
             contextByDataCenterId.UserName = "Administrator";
+            return contextByDataCenterId;
+        }//end static method
 
-            if (ctx == null) ctx = contextByDataCenterId;
+        public static Context CreateAdministratorFromCache(this Context ctx, string dataCenterId)
+        {
+            Context contextByDataCenterId = DataCenterService.GetDataCenterContextFromCache(dataCenterId);
+            contextByDataCenterId.UserId = FormConst.AdministratorID;
+            contextByDataCenterId.UserName = "Administrator";
             return contextByDataCenterId;
         }//end static method
 
