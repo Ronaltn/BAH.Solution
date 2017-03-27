d63e0be016641767d5d20811cc7774bd51d27031
Martin Wang <ronaltn@live.cn>
2017-03-27 10:10:10 +0800

    添加一些常用枚举

---
+++ Code/BAH.BOS.Core/LogicStatusConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BAH.BOS.Core
+{
+    /// <summary>
+    /// 逻辑状态常量。
+    /// </summary>
+    public class LogicStatusConstants
+    {
+        /// <summary>
+        /// 否。
+        /// </summary>
+        public static readonly string No = "A";
+
+        /// <summary>
+        /// 是。
+        /// </summary>
+        public static readonly string Yes = "B";
+    }
+}
