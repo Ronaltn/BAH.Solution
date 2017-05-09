1ce9ce30186bbe2449324d4cc67a14d2e9b25753
Martin Wang <ronaltn@live.cn>
2017-05-09 12:03:37 +0800

    True or False

---
+++ Code/BAH.Extension/BoolExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class BoolExtension
+    {
+        public static bool True(this bool b, Action action)
+        {
+            if (b) action?.Invoke();
+            return b;
+        }
+
+        public static bool False(this bool b, Action action)
+        {
+            if (!b) action?.Invoke();
+            return b;
+        }
+
+    }//end static class
+}//end namespace
