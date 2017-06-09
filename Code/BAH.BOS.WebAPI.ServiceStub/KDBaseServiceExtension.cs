6b3620bbdd75dbfcd0f78d878a4108f1b82b6ccc
Martin Wang <ronaltn@live.cn>
2017-06-09 10:02:27 +0800

    配合协同，组件式引用。

--- Code/BAH.BOS.WebAPI.ServiceStub/KDBaseServiceExtension.cs
+++ Code/BAH.BOS.WebAPI.ServiceStub/KDBaseServiceExtension.cs
@@ -1,4 +1,4 @@
-﻿using Kingdee.BOS.WebApi.ServicesStub;
+﻿using Kingdee.BOS.ServiceFacade.KDServiceFx;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,18 +9,18 @@ namespace BAH.BOS.WebAPI.ServiceStub
     /// <summary>
     /// API基类扩展。
     /// </summary>
-    public static class AbstractWebApiBusinessServiceExtension
+    public static class KDBaseServiceExtension
     {
         /// <summary>
         /// 登录会话是否过期。
         /// </summary>
-        /// <param name="businessService">API业务服务对象。</param>
+        /// <param name="service">API业务服务对象。</param>
         /// <param name="result">服务返回结果对象。</param>
         /// <param name="failedText">会话过期文本描述。</param>
         /// <returns>返回是否过期。</returns>
-        public static bool IsContextExpired(this AbstractWebApiBusinessService businessService, ServiceResult result, string failedText = "登录会话已过期！")
+        public static bool IsContextExpired(this KDBaseService service, ServiceResult result, string failedText = "登录会话已过期！")
         {
-            if (businessService.KDContext.Session.AppContext == null)
+            if (service.KDContext.Session.AppContext == null)
             {
                 result.Code = (int)ResultCode.Fail;
                 result.Message = failedText;
