0567fd2937ed3edc24ab80e71274840610cc1f35
Martin Wang <ronaltn@live.cn>
2017-12-30 22:43:37 +0800

    将常用的上传和忽略创建关联并入

---
+++ Code/BAH.BOS.App.ServicePlugIn/ConvertRule/IgnoreCreateLink.cs
@@ -0,0 +1,20 @@
+﻿using Kingdee.BOS.Core.Metadata.ConvertElement.PlugIn;
+using Kingdee.BOS.Core.Metadata.ConvertElement.PlugIn.Args;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace BAH.BOS.App.ServicePlugIn.ConvertRule
+{
+    [Description("单据转换插件，忽略创建关联数据。")]
+    public class IgnoreCreateLink : AbstractConvertPlugIn
+    {
+        public override void OnCreateLink(CreateLinkEventArgs e)
+        {
+            //不创建Link表数据。
+            e.Cancel = true;
+        }
+    }
+}
---
+++ Code/BAH.BOS.App.ServicePlugIn/FormOperation/Upload.cs
@@ -0,0 +1,124 @@
+﻿using BAH.BOS.Core;
+using Kingdee.BOS.Core.DynamicForm;
+using Kingdee.BOS.Core.DynamicForm.PlugIn;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+using Kingdee.BOS.Core.Interaction;
+using Kingdee.BOS.Core.Metadata.FieldElement;
+using Kingdee.BOS.Orm.DataEntity;
+using Kingdee.BOS.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace BAH.BOS.App.ServicePlugIn.FormOperation
+{
+    [Description("表单操作插件，上传操作。")]
+    public class Upload : AbstractOperationServicePlugIn
+    {
+        public override void OnPrepareOperationServiceOption(OnPrepareOperationServiceEventArgs e)
+        {
+            base.OnPrepareOperationServiceOption(e);
+            this.Option.SetIgnoreWarning(true);
+            this.Option.SetIgnoreInteractionFlag(true);
+        }
+
+        public override void OnPreparePropertys(PreparePropertysEventArgs e)
+        {
+            base.OnPreparePropertys(e);
+            e.FieldKeys.Add(this.BusinessInfo.GetForm().DocumentStatusFieldKey);
+        }
+
+        public override void BeforeExecuteOperationTransaction(BeforeExecuteOperationTransaction e)
+        {
+            base.BeforeExecuteOperationTransaction(e);
+            var documentStatusField = this.BusinessInfo.GetField(this.BusinessInfo.GetForm().DocumentStatusFieldKey).AsType<BillStatusField>();
+
+            //如果数据状态是审核中，需先执行撤销操作。
+            {
+                var dataEntities = e.SelectedRows
+                                    .Select(row => row.DataEntity)
+                                    .Where(data => data.FieldProperty<string>(documentStatusField).EqualsIgnoreCase(BillStatusConstant.DocumentStatus.Approving))
+                                    .ToArray();
+                if (dataEntities.Any())
+                {
+                    var result = dataEntities.CancelAssign(this.Context, this.BusinessInfo, this.Option);
+                    if (!result.IsSuccess)
+                    {
+                        e.Cancel = true;
+                        e.CancelMessage = string.Concat(e.CancelMessage, result.GetResultMessage());
+                        this.OperationResult.MergeResult(result);
+                    }//end if
+                }//end if
+            }
+
+            //如果数据状态是已审核，需先执行反审核操作。
+            {
+                var dataEntities = e.SelectedRows
+                                    .Select(row => row.DataEntity)
+                                    .Where(data => data.FieldProperty<string>(documentStatusField).EqualsIgnoreCase(BillStatusConstant.DocumentStatus.Approved))
+                                    .ToArray();
+                if (dataEntities.Any())
+                {
+                    var result = dataEntities.UnAudit(this.Context, this.BusinessInfo, this.Option);
+                    if (!result.IsSuccess)
+                    {
+                        e.Cancel = true;
+                        e.CancelMessage = string.Concat(e.CancelMessage, result.GetResultMessage());
+                        this.OperationResult.MergeResult(result);
+                    }//end if
+                }//end if
+            }
+
+            //如果无需中断，则执行暂存+保存操作。
+            if (!e.Cancel)
+            {
+                var dataEntities = e.SelectedRows
+                                    .Select(row => row.DataEntity)
+                                    .ToArray();
+
+                //记下重新审核的数据
+                var reCreatedDataEntities = dataEntities.Where(data => data.FieldProperty<string>(documentStatusField).EqualsIgnoreCase(BillStatusConstant.DocumentStatus.ReCreated))
+                                                        .ToArray();
+
+                //暂存
+                dataEntities.ForEach(data => documentStatusField.DynamicProperty.SetValue(data, BillStatusConstant.DocumentStatus.Draft));
+                dataEntities.Draft(this.Context, this.BusinessInfo, this.Option).Adaptive(result =>
+                {
+                    if (!result.IsSuccess) this.OperationResult.MergeResult(result);
+                });
+
+                //保存
+                reCreatedDataEntities.ForEach(data => documentStatusField.DynamicProperty.SetValue(data, BillStatusConstant.DocumentStatus.Created));
+                dataEntities.Save(this.Context, this.BusinessInfo, this.Option).Adaptive(result =>
+                {
+                    if (!result.IsSuccess)
+                    {
+                        e.Cancel = true;
+                        e.CancelMessage = string.Concat(e.CancelMessage, result.GetResultMessage());
+                        this.OperationResult.MergeResult(result);
+                    }//end if
+                });
+            }//end if
+        }//end method
+
+        public override void BeginOperationTransaction(BeginOperationTransactionArgs e)
+        {
+            base.BeginOperationTransaction(e);
+
+            //提交
+            e.DataEntitys.Submit(this.Context, this.BusinessInfo, this.Option).Adaptive(result =>
+            {
+                if (!result.IsSuccess) this.OperationResult.MergeResult(result);
+            }).ThrowWhenUnSuccess(result => result.GetResultMessage());
+
+            //审核
+            e.DataEntitys.Audit(this.Context, this.BusinessInfo, this.Option).Adaptive(result =>
+            {
+                if (!result.IsSuccess) this.OperationResult.MergeResult(result);
+            }).ThrowWhenUnSuccess(result => result.GetResultMessage());
+        }
+
+    }//end class
+}//end namespace
