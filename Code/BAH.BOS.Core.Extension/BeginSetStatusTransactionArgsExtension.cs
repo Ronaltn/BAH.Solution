a67dc6a2934812200d5dccd25fa4efdc54f0c9ed
Martin Wang <ronaltn@live.cn>
2018-02-06 14:54:46 +0800

    操作插件里获取选中的单据体

---
+++ Code/BAH.BOS.Core.Extension/BeginSetStatusTransactionArgsExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kingdee.BOS.Core.DynamicForm.PlugIn.Args
+{
+    public static class BeginSetStatusTransactionArgsExtension
+    {
+        public static BeginSetStatusTransactionArgs AsBeginSetStatusTransactionArgs(this BeginOperationTransactionArgs args)
+        {
+            return args.AsType<BeginSetStatusTransactionArgs>();
+        }
+    }
+}
---
+++ Code/BAH.BOS.Core.Extension/EndOperationTransactionArgsExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kingdee.BOS.Core.DynamicForm.PlugIn.Args
+{
+    public static class EndOperationTransactionArgsExtension
+    {
+        public static EndSetStatusTransactionArgs AsEndSetStatusTransactionArgs(this EndOperationTransactionArgs args)
+        {
+            return args.AsType<EndSetStatusTransactionArgs>();
+        }
+    }
+}
