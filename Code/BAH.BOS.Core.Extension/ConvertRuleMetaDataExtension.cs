c4fde029772bc832738ed0747b7dc441909885a3
Martin Wang <ronaltn@live.cn>
2018-07-30 10:26:46 +0800

    ConvertRuleMetaDataExtension

---
+++ Code/BAH.BOS.Core.Extension/ConvertRuleMetaDataExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kingdee.BOS.Core.Metadata.ConvertElement
+{
+    public static class ConvertRuleMetaDataExtension
+    {
+        public static T GetConvertPolicyElement<T>(this ConvertRuleElement element) where T : ConvertPolicyElement
+        {
+            return element.Policies.Where(police => police is T)
+                                   .Cast<T>()
+                                   .FirstOrDefault();
+        }
+
+        public static DefaultConvertPolicyElement GetDefaultConvertPolicyElement(this ConvertRuleElement element)
+        {
+            return GetConvertPolicyElement<DefaultConvertPolicyElement>(element);
+        }
+    }
+}
