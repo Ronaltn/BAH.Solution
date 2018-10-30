b1455616e8f65fedf8c2987d851356a94e904afc
Martin Wang <ronaltn@live.cn>
2018-10-30 17:43:34 +0800

    throw

--- Code/BAH.BOS.Core.Extension/ConvertRuleMetaDataExtension.cs
+++ Code/BAH.BOS.Core.Extension/ConvertRuleMetaDataExtension.cs
@@ -7,6 +7,11 @@ namespace Kingdee.BOS.Core.Metadata.ConvertElement
 {
     public static class ConvertRuleMetaDataExtension
     {
+        public static void ThrowWhenNull(this ConvertRuleElement element)
+        {
+            if (element == null) throw new KDBusinessException(string.Empty, "未配置有效的单据转换规则！");
+        }
+
         public static T GetConvertPolicyElement<T>(this ConvertRuleElement element) where T : ConvertPolicyElement
         {
             return element.Policies.Where(police => police is T)
